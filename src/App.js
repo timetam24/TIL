afad19c2d7c5da0d73bec9aacfb241e1329b3503
timetam <timetam24@naver.com>
2023-12-12 02:20:49 +0900

    react-easy-crop으로 이미지 크롭하기 예제 코드 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,82 @@
-import React from "react";
-// import ImageCrop from "./ImageCrop";
-import EasyCrop from "./EasyCrop";
-
 import "./App.css";
+import { useState } from "react";
+import ImageCropDialog from "./ImageCropDialog";
 
-export default function App() {
+const initData = [
+  {
+    id: 1,
+    imageUrl: "images/easyCrop/flower1.jpg",
+    croppedImageUrl: null,
+  },
+  {
+    id: 2,
+    imageUrl: "images/easyCrop/flower2.jpg",
+    croppedImageUrl: null,
+  },
+  {
+    id: 3,
+    imageUrl: "images/easyCrop/flower3.jpg",
+    croppedImageUrl: null,
+  },
+  {
+    id: 4,
+    imageUrl: "images/easyCrop/flower4.jpg",
+    croppedImageUrl: null,
+  },
+];
+
+function App() {
+  const [flowers, setFlowers] = useState(initData);
+  const [selectedFlower, setSelectedFlower] = useState(null);
+
+  const onCancel = () => {
+    setSelectedFlower(null);
+  };
+
+  const setCroppedImageFor = (id, crop, zoom, aspect, croppedImageUrl) => {
+    const newflowersList = [...flowers];
+    const flowerIndex = flowers.findIndex((x) => x.id === id);
+    const flower = flowers[flowerIndex];
+    const newFlower = { ...flower, croppedImageUrl, crop, zoom, aspect };
+    newflowersList[flowerIndex] = newFlower;
+    setFlowers(newflowersList);
+    setSelectedFlower(null);
+  };
+
+  const resetImage = (id) => {
+    setCroppedImageFor(id);
+  };
+
   return (
-    <div className="App">
-      {/* <ImageCrop></ImageCrop> */}
-      <EasyCrop></EasyCrop>
+    <div>
+      {selectedFlower ? (
+        <ImageCropDialog
+          id={selectedFlower.id}
+          imageUrl={selectedFlower.imageUrl}
+          cropInit={selectedFlower.crop}
+          zoomInit={selectedFlower.zoom}
+          aspectInit={selectedFlower.aspect}
+          onCancel={onCancel}
+          setCroppedImageFor={setCroppedImageFor}
+          resetImage={resetImage}
+        />
+      ) : null}
+      {flowers.map((flower) => (
+        <div className="imageCard" key={flower.id}>
+          <img
+            src={
+              flower.croppedImageUrl ? flower.croppedImageUrl : flower.imageUrl
+            }
+            alt=""
+            onClick={() => {
+              console.log(flower);
+              setSelectedFlower(flower);
+            }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
+
+export default App;
