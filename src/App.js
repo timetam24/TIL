5e0a8115b8cb1020aca5bf074db2e83530c5d921
timetam <timetam24@naver.com>
2023-12-11 12:41:57 +0900

    react-easy-crop 사용하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ImageCrop from "./ImageCrop";
+// import ImageCrop from "./ImageCrop";
+import EasyCrop from "./EasyCrop";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <ImageCrop></ImageCrop>
+      {/* <ImageCrop></ImageCrop> */}
+      <EasyCrop></EasyCrop>
     </div>
   );
 }
