528953bf77bd62fbbd0832671008c1286d184be7
timetam <timetam24@naver.com>
2024-03-11 10:16:18 +0900

    자주 사용할 color와 font 설정

--- practices/practice5/tailwind.config.ts
+++ practices/practice5/tailwind.config.ts
@@ -12,7 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      },colors: {
+        primary: `rgb(var(--color-primary))`,
+        secondary: `rgb(var(--color-secondary))`,
+        tertiary: `rgb(var(--color-tertiary))`,
+      }, 
     },
   },
   plugins: [],
