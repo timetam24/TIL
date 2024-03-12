acc79bd52c468624b4eb455d8bfc836dc8cf1bcb
timetam <timetam24@naver.com>
2024-03-12 09:09:19 +0900

    컴포넌트 간에 state 공유하기

--- practices/practice5/app/ui/form.tsx
+++ practices/practice5/app/ui/form.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import { Button } from "@/stories/Button";
+import { useState } from "react";
+import styles from "./form.module.css";
 
 export default function Form() {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
-    <>
-      <Button label="Click me!" primary={true} />
-    </>
+    <form className={styles.form}>
+      <MyButton onClick={handleCount} count={count} />
+      <MyButton onClick={handleCount} count={count} />
+    </form>
+  );
+}
+
+function MyButton({ onClick, count }: { onClick: () => void; count: number }) {
+  return (
+    <div>
+      <Button label={`Clicked ${count}`} primary={true} onClick={onClick} />
+    </div>
   );
 }
