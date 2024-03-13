474d20914a0f300fc820d01bf17ff16125c28e56
timetam <timetam24@naver.com>
2024-03-13 10:04:50 +0900

    더미 데이터 파일 분리

--- practices/practice5/app/page.tsx
+++ practices/practice5/app/page.tsx
@@ -1,15 +1,6 @@
 import FilterableProductTable from "./ui/table";
 import styles from "./page.module.css";
-import { Product } from "./type";
-
-const PRODUCTS: Product[] = [
-  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
+import { PRODUCTS } from "./data";
 
 export default function Home() {
   return (
