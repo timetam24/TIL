28ac1ffaf1e8b632dc2e9bcd0440cdcc5bf124a9
timetam <timetam24@naver.com>
2024-03-19 11:02:17 +0900

    font 설정

---
+++ practices/practice6/src/lib/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Mr_De_Haviland } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+export const mrDeHaviland = Mr_De_Haviland({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-mr-de-haviland",
+});
--- practices/practice6/tailwind.config.ts
+++ practices/practice6/tailwind.config.ts
@@ -7,7 +7,12 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        haviland: ["var(--font-mr-de-haviland)"],
+      },
+    },
   },
   plugins: [],
 };
