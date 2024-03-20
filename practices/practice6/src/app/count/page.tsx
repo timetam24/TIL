d0adb07b58f02713ccb73e9fc2203a4bdc57b0fe
timetam <timetam24@naver.com>
2024-03-20 17:10:22 +0900

    State를 보존하고 초기화하기
    - isFancy와 상관없이 같은 자리의 같은 컴포넌트
      <Counter>는 CountPage 컴포넌트가 반환한
      <div>의 첫 번째 자식으로써 State를 보존한다.

--- practices/practice6/src/app/count/page.tsx
+++ practices/practice6/src/app/count/page.tsx
@@ -1,10 +1,39 @@
+"use client";
+
 import Counter from "@/containers/count/counter";
+import { useState } from "react";
 
 export default function CountPage() {
+  const [isFancy, setIsFancy] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsFancy(e.target.checked);
+  };
+
   return (
     <div className="flex gap-12 justify-center min-h-screen items-center bg-black">
-      <Counter /> {/* 독립된 state */}
-      <Counter /> {/* 독립된 state */}
+      {isFancy ? <Counter isFancy={true} /> : <Counter isFancy={false} />}
+      {/* ↑ 같은 자리의 같은 컴포넌트 ↑*/}
+      <Label isFancy={isFancy} onchange={handleChange} />
     </div>
+  );
+}
+
+interface LabelProps {
+  isFancy: boolean;
+  onchange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Label({ isFancy, onchange }: LabelProps) {
+  return (
+    <label className="text-white">
+      <input
+        type="checkbox"
+        className="font-inter mr-1"
+        checked={isFancy}
+        onChange={onchange}
+      />
+      Use Fancy Styling
+    </label>
   );
 }
--- practices/practice6/src/containers/count/counter.tsx
+++ practices/practice6/src/containers/count/counter.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import clsx from "clsx";
 
-export default function Counter() {
+export default function Counter({ isFancy }: { isFancy: boolean }) {
   const [score, setScore] = useState(0);
   const [hover, setHover] = useState(false);
 
@@ -18,7 +18,13 @@ export default function Counter() {
       onPointerEnter={() => setHover(true)}
       onPointerLeave={() => setHover(false)}
     >
-      <h1 className="font-inter font-extrabold text-5xl mt-20">{score}</h1>
+      <h1
+        className={clsx("font-inter font-extrabold text-5xl mt-20", {
+          "text-red-700": isFancy,
+        })}
+      >
+        {score}
+      </h1>
       <button
         onClick={() => setScore(score + 1)}
         className="bg-black text-[#F2F2F2] px-4 py-1 rounded  font-uniform mt-auto mb-4 hover:bg-indigo-700"
