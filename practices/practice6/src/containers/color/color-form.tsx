2f3c92ef9c1638523576b6e2099228fd88eec76b
timetam <timetam24@naver.com>
2024-03-22 14:56:57 +0900

    useFormStatus, Form Action 사용해보기

--- practices/practice6/src/containers/color/color-form.tsx
+++ practices/practice6/src/containers/color/color-form.tsx
@@ -5,10 +5,10 @@ import axios from "axios";
 import clsx from "clsx";
 import { Toaster, toast } from "react-hot-toast";
 import { MdiHeart } from "@/lib/icons";
+import { useFormStatus } from "react-dom";
 
 export default function ColorForm() {
   const [color, setColor] = useState("");
-  const [status, setStatus] = useState("typing"); // 'typing', 'submitting', or 'success'
   const [error, setError] = useState(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,16 +25,13 @@ export default function ColorForm() {
     });
   };
 
-  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setStatus("submitting");
+  const submitForm = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     try {
       await axios.post("/api/color", { color });
-      setStatus("success");
       showSuccessMessage();
       setColor("");
     } catch (error: any) {
-      setStatus("typing");
       setError(error);
     }
   };
@@ -42,7 +39,7 @@ export default function ColorForm() {
   return (
     <form
       className="w-[40rem] h-80 py-8 px-6 bg-apricot rounded-md flex flex-col justify-between text-dark-green"
-      onSubmit={submitForm}
+      action={submitForm}
     >
       <Toaster />
       <h1 className="uppercase font-bold whitespace-pre-wrap text-4xl min-[542px]:w-[70%] min-[455px]:w-[85%] max-[455px]:w-[100%] max-[353px]:text-3xl">
@@ -50,30 +47,51 @@ export default function ColorForm() {
         <span className="font-haviland font-normal">c</span>
         olor<span className="font-haviland  font-normal">?</span>
       </h1>
-      <div className="self-end flex gap-4 text-xl">
+      <Form color={color} handleChange={handleChange} />
+      {error !== null && <p className="Error">{error}</p>}
+    </form>
+  );
+}
+
+function Form({
+  color,
+  handleChange,
+}: {
+  color: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  const { pending, data } = useFormStatus();
+
+  return (
+    <div className="font-inter self-end flex flex-col text-xl gap-1">
+      <div className="flex gap-4">
         <input
           type="text"
           placeholder="answer here..."
           className="text-inherit bg-inherit p-1 border-0 border-b-2 border-solid border-dark-green w-48 outline-0 focus:-translate-y-[1px] focus:border-b-2 focus:border-solid focus:border-dark-green transition-all duration-200 ease-in-out"
           value={color}
+          name="color"
           onChange={handleChange}
-          disabled={status === "submitting"}
+          disabled={pending}
         />
         <button
+          type="submit"
           className={clsx(
             "text-dark-green font-semibold bg-apricot py-1 px-2 border-2 border-solid border-dark-green rounded-md cursor-pointer  hover:bg-dark-green hover:text-apricot focus:bg-dark-green focus:text-apricot active:bg-dark-green active:text-apricot transition-all duration-200 ease-in-out  focus-visible:outline-none focus-visible:ring-offset-apricot focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-dark-green ",
             {
-              "bg-dark-green text-red-100": status === "typing",
+              "bg-dark-green text-red-100": color.length > 0,
               "disabled:pointer-events-none disabled:opacity-50":
-                color.length === 0,
+                color.length === 0 || pending,
             }
           )}
-          disabled={color.length === 0 || status === "submitting"}
+          disabled={color.length === 0 || pending}
         >
-          {status === "submitting" ? "Submitting..." : "Submit"}
+          {pending ? "Submitting..." : "Submit"}
         </button>
       </div>
-      {error !== null && <p className="Error">{error}</p>}
-    </form>
+      <p className="font-semibold">
+        {data ? `Sending ${data?.get("color")}...` : ""}
+      </p>
+    </div>
   );
 }
