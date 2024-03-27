3cec2813d919efead92078e632560f474d09e0a4
timetam <timetam24@naver.com>
2024-03-28 07:48:41 +0900

    Ref로 값 참조하기: 챌린지
    - 버튼 클릭 당시가 아닌 현재 입력된(최신의)
      텍스트를 추가하기 위해 Ref 사용하기

--- practices/practice6/src/containers/task/add-task.tsx
+++ practices/practice6/src/containers/task/add-task.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "./task-button";
 
 export default function AddTask({
@@ -9,21 +9,27 @@ export default function AddTask({
   onAddTask: (text: string) => void;
 }) {
   const [text, setText] = useState("");
+  const textRef = useRef(text);
+
+  const handleAdd = () => {
+    setTimeout(() => {
+      onAddTask(textRef.current);
+      setText("");
+    }, 3000);
+  };
+
   return (
     <div className="w-96 my-5 flex gap-2">
       <input
         placeholder="Add task"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value);
+          textRef.current = e.target.value;
+        }}
         className="px-2 py-1 rounded w-full"
       />
-      <Button
-        name="add"
-        onClick={() => {
-          setText("");
-          onAddTask(text);
-        }}
-      >
+      <Button name="add" onClick={handleAdd}>
         Add
       </Button>
     </div>
